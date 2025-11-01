ca068ba3cf71577a83d4e0bccf07e6d7393ba777
Michael Mukiibi <michael.mukiibi.k@gmail.com>
2025-11-01 15:12:46 +0300

    added service worker

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+// WORKER CACHE
+const CACHE_NAME = 'gemma-llm-model-v1'
+
+// ASSET PATH
+const MODEL_ASSET_PATH = "./ganda-gemma-1b-litert.task"
+
+// FILES TO PRECACHE
+const URLS = [
+    '/',
+    'index.html',
+    'main.js',
+    MODEL_ASSET_PATH,
+    '/wasm/llm_inference_api.wasm',
+]
+
+// 1. INSTALLATION
+self.addEventListener('install', (event) => {
+    // Log installation step
+    console.log('Service Worker: Installing and pre-caching assets.')
+
+    // Open the cache
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)))
+
+    self.skipWaiting()
+})
+
+// 2. FETCH REQUESTS
+self.addEventListener('fetch', (event) => {
+    // Cache GET requests only
+    if (event.request.method !== 'GET') return
+
+    event.respondWith(caches.match(event.request).then((response) => {
+        // Get from cache
+        if (response) return response
+
+        // Fetch from network
+        return fetch(event.request)
+    }))
+})
+
+// 3. CLEAN UP OLD CACHES
+self.addEventListener('activate', (event) => {
+    // Log cleanup
+    console.log('Service worker: Cleaning up old caches')
+
+    const CACHE_ALLOW_LIST = [CACHE_NAME]
+
+    event.waitUntil(caches.keys().then((cacheNames) => {
+        return Promise.all(cacheNames.map(cacheName => {
+            if (CACHE_ALLOW_LIST.indexOf(cacheName) === -1) return caches.delete(cacheName)
+        }))
+    }))
+})
